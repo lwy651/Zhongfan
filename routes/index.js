124ef75fadfe07c630c5ae27178f8500a925379f
xiao5 <a651@bogon.dhcp>
2016-08-13 00:31:21 +0800

    async await

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,27 @@ var router = require('koa-router')();
 const connectDB = require('../controls/connectDB');
 
 var usersDB = connectDB.users;
+
+var del = function(){
+	return 123;
+};
+
+var sleep = function(time){
+	return new Promise(function(resolve,reject){
+		setTimeout(function(){
+			resolve(del());
+		},time);
+	});
+};
+
 router.get('/', async function (ctx, next) {
 	usersDB.find(function(err,docs){
-		console.log(docs);
-	})
+		//console.log(docs);
+	});
+	var result = await sleep(10000,del);
+	console.log(result);
   	await ctx.render('index');
 })
+
+
 module.exports = router;
